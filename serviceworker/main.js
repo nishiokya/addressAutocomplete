5348464b4aa7d5466528a6bbe3e7f2e1bb4cc7a2
nishiokya <nishiokya@gmail.com>
2023-03-24 00:18:17 +0900

    serviceworkerの登録

--- serviceworker/main.js
+++ serviceworker/main.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(err) {
+      console.error('Service Worker registration failed:', err);
+    });
+  });
+}
+
 async function loadInputHistory() {
   return new Promise((resolve, reject) => {
     if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
---
+++ sw.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('Service Worker Registered');
+    })
+    .catch((error) => {
+      console.log('Service Worker Registration Failed: ', error);
+    });
+}
